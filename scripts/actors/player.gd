aa25c21ae23a6c85b14b2e85183d5bd937221b22
Blast <chienhuumtp45@gmail.com>
2025-02-06 15:45:06 +0700

    Implement player character with movement and jump mechanics; add demo world scene and external resources for materials and skeleton.

---
+++ scripts/actors/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+@onready var state_machine = $AnimationTree["parameters/playback"]
+
+var speed = 5.0
+var jump_speed = 4.5
+var mouse_sensitivity = 0.002
+var is_run = false
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		state_machine.travel("Jump_Idle")
+	
+	get_input()
+	move_and_slide()
+
+func get_input() -> void:
+	var input_dir := Input.get_vector("A", "D", "W", "S")
+	var movement_dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+	movement_dir = movement_dir.normalized()
+
+	velocity.x = movement_dir.x * speed
+	velocity.z = movement_dir.z * speed
+	
+	if input_dir != Vector2.ZERO:
+		is_run = true
+		state_machine.travel("Run")
+	elif is_on_floor():
+		is_run = false
+		state_machine.travel("Idle")
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+	
+	if event.is_action_pressed("ui_accept") and is_on_floor():
+		state_machine.travel("Jump_Idle")
+		velocity.y = jump_speed
